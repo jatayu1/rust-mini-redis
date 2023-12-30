396d2e03d547dbcc78cbd02cd54d221d4d7d7850
jatayu1 <dakash682@gmail.com>
2023-12-30 23:12:20 +0530

    shared state

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to adderss
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // The second item contains the IP and port of the new connections.
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
+        println!("Accepted");
         
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });  
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream,  db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // A hashmap is used to store data
-    let mut db = HashMap::new();
 
     // Connection, provided by `mini-redis`, handles parsing frames the socket
     let mut connection = Connection::new(socket);
@@ -30,15 +38,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap(){
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                //The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()){
-                    // `Frame::Bulk` expects data to be of type `Bytes`.
-                    // `&Vec<u8>` is converted to `Bytes` uising `into()`.
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
